8b5b0a206182adb64b0f21e7b8785a20a633e30a
Tiago Gomes <tiagogomes187@gmail.com>
2021-10-23 22:06:59 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -2,6 +2,9 @@ package com.devsuperior.hrworker.resources;
 
 import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +18,15 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static final Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    private final Environment env;
+
     private final WorkerRepository repository;
 
-    public WorkerResource(WorkerRepository repository) {
+    public WorkerResource(WorkerRepository repository, Environment env) {
         this.repository = repository;
+        this.env = env;
     }
 
     @GetMapping
@@ -29,6 +37,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
